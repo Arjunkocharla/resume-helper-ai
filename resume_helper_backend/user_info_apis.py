24a644f85b05cb9f33475cd9b6704fdcf4777b9a
ArjunKocharla <arjunkocharla98@gmail.com>
2024-10-14 20:49:41 -0400

    taking vars from env

--- resume_helper_backend/user_info_apis.py
+++ resume_helper_backend/user_info_apis.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+cred = credentials.Certificate(json.loads(os.getenv('FIREBASE_CONFIG')))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
